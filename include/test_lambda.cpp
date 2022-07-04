477f7b84771b40ac4438e04eb5afa100395a1173
Rraiscies <l1258712106@163.com>
2022-07-04 11:01:45 +0800

    update: test file

--- include/test_lambda.cpp
+++ include/test_lambda.cpp
@@ -5,8 +5,11 @@
 
 using namespace rais::meta;
 
+template <typename... Args>
+using l = lambda<op::get, expr<type_list, Args...>, param<0>>;
+
+debug_type<l<int, long, double, void, char>::eval<meta<3>>>;
 
 int main() {
-
 }
 
